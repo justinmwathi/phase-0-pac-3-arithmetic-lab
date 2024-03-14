9afad579289aa99a63d0be057b2f5b85f972eb41
justin <justinmwathi32@gmail.com>
2024-03-14 09:24:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+return (a+b);
+}
+add;
+
+function subtract(a,b){
+return(a-b);
+}
+subtract;
+
+function multiply(a,b){
+return(a*b);
+}
+multiply;
+
+function divide(a,b){
+return (a/b);
+}
+divide;
+
+function increment(a){
+return(a+=1);
+}
+increment;
+function decrement(a){
+return(a-=1)
+}
+decrement;
+function makeInt(n){
+    return(parseInt(n,10));
+}
+makeInt;
+function preserveDecimal(n){
+return(parseFloat(n))
+}
+preserveDecimal;
